71fe212f022db6878fc81c7c082733420605d6ce
JosephDomingo <josephddomingo@gmail.com>
2024-02-17 23:28:52 -0500

    initialized card
    added html
    added css
    added js

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var_envelope = $("#envelope");
+    var button_open = $("#open");
+    var button_reset = $("#reset");
+
+    envelope.click(function(){
+        open();
+    });
+
+    button_open.click(function(){
+        open();
+    });
+
+    button_reset.click(function(){
+        close();
+    });
+
+    function open(){
+        envelope.addClass("open")
+            .removeClass("close");
+    }
+
+    function close(){
+        envelope.addClass("close")
+            .removeClass("open");
+    }
+
+})
+
+
+
